1d0fa7fc1f06a57d18d90a96f3369c12c780169e
yuinore <yuinore@hotmail.co.jp>
2020-04-25 17:17:43 -0400

    スプレッドシートからseed更新できるように

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,53 +1,74 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 Work.destroy_all
-work1 = Work.create(
-  name: "チノちゃん つかまえた！",
-  caption: [
-      "よそのこハンターえちきゅねシリーズ 番外編 vol.2",
-      "2019/11/3",
-      "Author: hato",
-      "Guest: ラクシュみぃ, からたに",
-    ].join("\n"),
-)
-work2 = Work.create(
-  name: "Strawberry Mint Chocolate",
-  caption: [
-      "Music CD",
-      "2015/10/25 (M3 2015秋)",
-      "Author: Yu^ta, yuinore, hato, mizninjin",
-    ].join("\n"),
-)
-work3 = Work.create(
-  name: "城主の時代だ！",
-  caption: "Music Video",
-)
-
 Image.destroy_all
-Image.create([
-  name: "チノちゃん つかまえた！" ,
+work = Work.create(
+         name: "チノちゃん つかまえた！",
+         caption: "同人誌\n2019/11/3\nAuthor: hato\nGuest: ラクシュみぃ, からたに",
+       )
+Image.create(
+  name: "チノちゃん つかまえた！_0",
   index: 0,
-  path: "books/chino_dl_001.png",
+  path: "images/books_007_chino.png",
   link: "https://www.melonbooks.co.jp/detail/detail.php?product_id=579768",
-  work: work1,
-])
-Image.create([
-  name: "Strawberry Mint Chocolate" ,
+  work: work,
+)
+work = Work.create(
+         name: "城主の時代だ！",
+         caption: "映像作品\n2018/12/22\nAuthor: yuinore, Yu^ta\nGuest: furea2",
+       )
+Image.create(
+  name: "城主の時代だ！_0",
   index: 0,
-  path: "jackets/strawberry-mint-chocolate-jacket.png",
-  link: nil,
-  work: work2,
-])
-Image.create([
-  name: "城主の時代だ！" ,
-  index: 0,
-  path: "movies/castle_age.jpg",
+  path: "images/movie_006_castleage.jpg",
   link: "https://www.youtube.com/watch?v=vU8lj_drm9A",
-  work: work3,
-])
+  work: work,
+)
+work = Work.create(
+         name: "いちご みんと ちょこれーと",
+         caption: "映像作品\n2017/2/14\nAuthor: yuinore, Yu^ta, mizninjin, hato\nGuest: バリキオス, ちゃば, 守鈴, Azel",
+       )
+Image.create(
+  name: "いちご みんと ちょこれーと_0",
+  index: 0,
+  path: "images/movie_002_smc_00.jpg",
+  link: "https://www.youtube.com/watch?v=SV_BxJzKcnY",
+  work: work,
+)
+Image.create(
+  name: "いちご みんと ちょこれーと_1",
+  index: 1,
+  path: "images/movie_002_smc_01.jpg",
+  link: nil,
+  work: work,
+)
+Image.create(
+  name: "いちご みんと ちょこれーと_2",
+  index: 2,
+  path: "images/movie_002_smc_02.jpg",
+  link: nil,
+  work: work,
+)
+Image.create(
+  name: "いちご みんと ちょこれーと_3",
+  index: 3,
+  path: "images/movie_002_smc_03.jpg",
+  link: nil,
+  work: work,
+)
+Image.create(
+  name: "いちご みんと ちょこれーと_4",
+  index: 4,
+  path: "images/movie_002_smc_04.jpg",
+  link: nil,
+  work: work,
+)
+work = Work.create(
+         name: "Strawberry Mint Chocolate",
+         caption: "音楽CD\n2015/10/25 (M3 2015秋)\nAuthor: Yu^ta, mizninjin, yuinore, hato",
+       )
+Image.create(
+  name: "Strawberry Mint Chocolate_0",
+  index: 0,
+  path: "images/cd_001_smc.jpg",
+  link: "https://booth.pm/ja/items/1344613",
+  work: work,
+)
---
+++ lib/tasks/seeder.rake
@@ -0,0 +1,35 @@
+namespace :seeder do
+  desc "db/seed.rb を更新します"
+  task :run do
+    url = "https://script.google.com/macros/s/AKfycbxi2V2ZeVwl2Icq13W2lu_yifd0MTKdSJX0VamK4qvECkdI8Tc/exec"
+    json = `curl -L -s "#{url}"`
+    obj = JSON.parse json, symbolize_names: true
+    str = []
+
+    str << "Work.destroy_all"
+    str << "Image.destroy_all"
+
+    obj.reject { |work| work[:name].blank? }.
+        sort_by { |work| p work[:published_at] }.
+        reverse.
+        each { |work|
+      str << "work = Work.create("
+      str << "         name: #{work[:name].inspect},"
+      str << "         caption: #{work[:caption].inspect},"
+      str << "       )"
+
+      work[:images].each_line.map(&:chomp).each_with_index { |image, i|
+        str << "Image.create("
+        str << "  name: #{(work[:name] + "_#{i}").inspect},"
+        str << "  index: #{i},"
+        str << "  path: #{("images/" + image).inspect},"
+        str << "  link: #{i == 0 && work[:link].present? ? work[:link].inspect : "nil"},"
+        str << "  work: work,"
+        str << ")"
+      }
+    }
+
+    puts str.join("\n")
+    File.write("db/seeds.rb", str.join("\n"))
+  end
+end
