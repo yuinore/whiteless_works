1dc4c3981ec2d792030a4ebd54cb512e8c8b858f
yuinore <yuinore@hotmail.co.jp>
2020-05-02 18:09:27 -0400

    retina 対応

--- app/assets/javascripts/root.js
+++ app/assets/javascripts/root.js
@@ -21,6 +21,12 @@ $(function(){
     type: 'image',
     gallery: {
       enabled: true
+    },
+    retina: {
+      ratio: 2,
+      replaceSrc: function(item, _ratio) {
+        return item.src.replace(/\.[^.]+$/, function(ext) { return "@2x" + ext; });
+      }
     }
   });
 });
