15db44fd650121663163d1f1329b55148cc50922
yuinore <yuinore@hotmail.co.jp>
2022-03-24 01:25:41 +0900

    Generate Static Site

---
+++ assets/root-f29da36ba397e1b492baedcb1817f6c55aaa30c65cdc5820747df2914373b382.js
@@ -0,0 +1,26 @@
+function update_fadein(e, offset = 100){
+  $('.picture').each(function(){
+    var targetElement = $(this).offset().top;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll > targetElement - windowHeight + offset){
+      $(this).css('opacity','1');
+      $(this).css('transform','translateY(0)');
+    }
+  });
+}
+
+$(function(){
+    $(window).scroll(update_fadein);
+    update_fadein(null, 0);
+});
+
+$(function(){
+  $('.image-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+  });
+});
